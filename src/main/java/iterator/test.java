7fbc1c62a39492bf8194977efb4de76729036d03
X-ljy <17637938901@163.com>
2019-05-17 16:53:36 +0800

    init

--- src/main/java/iterator/test.java
+++ src/main/java/iterator/test.java
@@ -6,6 +6,6 @@ package iterator;
  */
 public class test {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hello DesignPattern");
     }
 }
