2a3883546f1587b7e321aec48f3fa9e39c2c85b4
X-ljy <17637938901@163.com>
2019-08-15 20:49:01 +0800

    观察者模式

--- src/main/java/com/ljy/learn/observer/DigitObserver.java
+++ src/main/java/com/ljy/learn/observer/DigitObserver.java
@@ -5,6 +5,7 @@ package com.ljy.learn.observer;
  * @date : 2019/8/15
  */
 public class DigitObserver implements Observer {
+
     @Override
     public void update(NumberGenerator generator) {
         System.out.println("DigitObserver : " + generator.getNumber());
